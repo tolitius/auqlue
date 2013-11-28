b33433c4faf96ecbd7043f849431e10c39755ff8
anatoly <tolitius@gmail.com>
2013-11-28 17:40:40 -0500

    css'ing qa/ask part. in progress

--- src/auqlue/layout/qa.clj
+++ src/auqlue/layout/qa.clj
@@ -5,9 +5,9 @@
 (defn header [{:keys [title date sname cname]}]
   (in-container {:container-class "qa" :jumbo-class "qa-header"}
     [:div.row.center
-     [:p.qa-title-header title]
-     (if cname [:p.qa-conf-name cname])
-     [:p.qa-date date]]))
+     (if cname [:span.qa-conf-name cname])
+     [:span.qa-title-header title]
+     [:span.qa-date date]]))
 
 (defn address-speaker [sname]
   (if sname
@@ -28,21 +28,23 @@
        [:input.form-control {:id "q-input" :autofocus "autofocus" :placeholder "e.g. what is, why, how to, where, when, etc.."}]]]]))
 
 (defn questions [{:keys [pid]}]
-  (in-container {:container-class "create-auqlue" :jumbo-class "o-footer"}
+  (in-container {:container-class "qa" :jumbo-class "qa-footer"}
     [:div.row.center
-     [:form {:role "form"}
-      ;; [:div.form-group.col-md-6.col-md-offset-3.o-label
-      ;;     [:span.label.label-success "optional"]]
-      [:div.form-group.col-md-6.col-md-offset-3
+      [:div.col-md-6.col-md-offset-3
        [:div.panel.panel-warning 
-        [:div.panel-body.panel-heading
-          [:form.form-inline.center {:role "form"}
-           [:div.form-group.col-md-6
-            [:label {:for "presenter-name"} "Presenter's Name"]
-            [:input.form-control {:id "presenter-input" :placeholder "\"address\" the presenter"}]]
-           [:div.form-group.col-md-6
-            [:label {:for "conference-name"} "Conference Name"]
-            [:input.form-control {:id "conference-input" :placeholder "relate it to the conference"}]]]]]]]]))
+        [:div.panel-body.panel-heading.col-md-12.q-panel
+          [:div.vote-up.col-md-1 
+           [:div [:i.fa.fa-chevron-up]]
+           [:div.v-rank "42"]]
+          [:div.col-md-10.q-text [:span "WTF is Monad?"]]]]]]
+    [:div.row.center
+      [:div.col-md-6.col-md-offset-3
+       [:div.panel.panel-warning 
+        [:div.panel-body.panel-heading.col-md-12.q-panel
+          [:div.vote-up.col-md-1 
+           [:div [:i.fa.fa-chevron-up]]
+           [:div.v-rank "23"]]
+          [:div.col-md-10.q-text [:span "This would be a long question, so let me start by saying.."]]]]]]))
 
 (defn qa-for [{:keys [date title sname cname] :as about}]
   (with-bootstrap (str "auqlue: " title)
